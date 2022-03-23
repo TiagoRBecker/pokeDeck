ed92f298bc05bf222806ff93103d7f2787dd2cee
Tiago Becker <beckertiago09@gmail.com>
2022-03-23 18:51:05 -0300

    correcoes

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     
+     <>
       <Image src={"/images/bg.jpg"} width={1480} height={630} alt="bg"/>
-     
+      </>
     
   )
 }
