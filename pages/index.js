5b103af57fddff02d6b065debf6bf31fdb9f0ef7
Tiago Becker <beckertiago09@gmail.com>
2022-03-23 18:48:56 -0300

    correcoes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "../styles/Index.module.css"
+
 export default function Home() {
   return (
-     <div className={styles.container}>
+     
       <Image src={"/images/bg.jpg"} width={1480} height={630} alt="bg"/>
-     </div>
+     
     
   )
 }
